2816df10393d1ebcc570401e8e15de7e4f59ab88
vivyuan <moyuan@ucdavis.edu>
2021-02-11 20:00:23 -0800

    new

---
+++ des157/studio2/scripts.js
@@ -0,0 +1,21 @@
+
+
+function change1() {
+    document.getElementById("changingImage").innerHTML='<img src = images/m1.png>';
+}
+
+function change2() {
+    document.getElementById("changingImage").innerHTML='<img src = images/m2.png>';
+}
+
+function change3() {
+    document.getElementById("changingImage").innerHTML='<img src = images/m3.png>';
+}
+
+function change4() {
+    document.getElementById("changingImage").innerHTML='<img src = images/m4.png>';
+}
+
+function change5() {
+    document.getElementById("changingImage").innerHTML='<img src = images/m5.png>';
+}
