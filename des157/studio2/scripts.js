4997faa0086e59f6f4831df63ed2554238cf7e92
vivyuan <moyuan@ucdavis.edu>
2021-02-11 22:46:00 -0800

    almost finished

--- des157/studio2/scripts.js
+++ des157/studio2/scripts.js
@@ -1,21 +1,30 @@
+(function(){
+    'use strict'
 
+    document.getElementById('firstItem').addEventListener('click', change1);
+    document.getElementById('secondItem').addEventListener('click', change2);
+    document.getElementById('thirdItem').addEventListener('click', change3);
+    document.getElementById('fourthItem').addEventListener('click', change4);
+    document.getElementById('fifthItem').addEventListener('click', change5);
 
-function change1() {
-    document.getElementById("changingImage").innerHTML='<img src = images/m1.png>';
-}
+    function change1() {
+        document.getElementById("changingImage").innerHTML='<img src = images/m3.png>';
+    }
 
-function change2() {
-    document.getElementById("changingImage").innerHTML='<img src = images/m2.png>';
-}
+    function change2() {
+        document.getElementById("changingImage").innerHTML='<img src = images/m1.png>';
+    }
 
-function change3() {
-    document.getElementById("changingImage").innerHTML='<img src = images/m3.png>';
-}
+    function change3() {
+        document.getElementById("changingImage").innerHTML='<img src = images/m2.png>';
+    }
 
-function change4() {
-    document.getElementById("changingImage").innerHTML='<img src = images/m4.png>';
-}
+    function change4() {
+        document.getElementById("changingImage").innerHTML='<img src = images/m4.png>';
+    }
 
-function change5() {
-    document.getElementById("changingImage").innerHTML='<img src = images/m5.png>';
-}
+    function change5() {
+        document.getElementById("changingImage").innerHTML='<img src = images/m5.png>';
+    }
+
+}());
