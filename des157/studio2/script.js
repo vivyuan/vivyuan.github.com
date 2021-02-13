22f33555b2aab6bdfa9a8c348c582d7e0da80c69
vivyuan <moyuan@ucdavis.edu>
2021-02-12 18:22:34 -0800

    add comment

--- des157/studio2/script.js
+++ des157/studio2/script.js
@@ -1,11 +1,16 @@
 (function(){
     'use strict'
 
+    // Make the five suglasses clickable
+    
     document.getElementById('firstItem').addEventListener('click', change1);
     document.getElementById('secondItem').addEventListener('click', change2);
     document.getElementById('thirdItem').addEventListener('click', change3);
     document.getElementById('fourthItem').addEventListener('click', change4);
     document.getElementById('fifthItem').addEventListener('click', change5);
+
+    // Add function to each clickable suglasses
+    // When each item is clicked, the main image change to the image that is wearing the clicked item
 
     function change1() {
         document.getElementById("changingImage").innerHTML='<img src = images/m3.png>';
