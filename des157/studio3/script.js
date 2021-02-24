59e7ced00e8275c577bdbf93721e16903ec5625d
vivyuan <moyuan@ucdavis.edu>
2021-02-23 16:21:13 -0800

    add comments

--- des157/studio3/script.js
+++ des157/studio3/script.js
@@ -7,11 +7,9 @@
 	const game = document.getElementById('game');
 	const score = document.getElementById('score');
 	const actionArea = document.getElementById('actions');
-    const clickSound = new Audio('media/clicksound.mp3');
-    const startSound = new Audio('media/startsound.mp3');
-    const winningSound = new Audio('media/winningsound.mp3');
-
-
+    const clickSound = new Audio('media/clicksound.mp3');  //sound made by myself and edited on iMovie
+    const startSound = new Audio('media/startsound.mp3');  //sound made by myself on Garageband
+    const winningSound = new Audio('media/winningsound.mp3'); //sound made by myself on Garageband
 
     const gameData = {
 	dice: ['images/1die.jpg', 'images/2die.jpg', 'images/3die.jpg', 
@@ -38,6 +36,7 @@
         setUpTurn();
     });
 
+    // step 1: randomly select who's turn to play
     function setUpTurn() { 
         game.innerHTML = `<h5>Roll the dice for the ${gameData.players[gameData.index]}</h5>`; 
         actionArea.innerHTML = '<button id="roll">Roll the Dice</button>'; 
@@ -47,6 +46,7 @@
          });
     }
 
+    // step 2: the selected player goes first, and game started
     function throwDice(){ 
         actionArea.innerHTML = ' '; 
         gameData.roll1 = Math.floor(Math.random() * 6) + 1; 
@@ -57,7 +57,7 @@
         gameData.rollSum = gameData.roll1 + gameData.roll2; 
         console.log(gameData);
 
-        // if two l's are rolled
+        // if two 1's are rolled
         if( gameData.rollSum === 2 ){ 
             game.innerHTML += '<h6>Oh snap! Snake eyes!</h6>';
             gameData.score[gameData.index] = 0; 
@@ -93,6 +93,7 @@
         } 
     }
 
+    // check which player wins
     function checkWinningCondition(){ 
         if(gameData.score[gameData.index] > gameData.gameEnd){ 
             score.innerHTML = `<h4>${gameData.players[gameData.index]} wins with ${gameData.score[gameData.index]} points!</h4>`; 
@@ -104,6 +105,7 @@
         } 
     }
 
+    // score counted for both players
     function showCurrentScore() { 
         score.innerHTML =  `<p><h3>Current Score<h3> <strong id="playerA">${gameData.players[0]} 
             <h4>${gameData.score[0]}</h4></strong> <strong id="playerB">${gameData.players[1]} <h4>${gameData.score[1]}<h4></strong></p>`; 
