0af1321ed54e66358dcb3ff9ffee00fe822e844b
vivyuan <moyuan@ucdavis.edu>
2021-03-04 17:37:13 -0800

    usertest

--- des157/final/usertest/script.js
+++ des157/final/usertest/script.js
@@ -17,7 +17,7 @@
 
         // Below are for the results
         if(noun1 && noun2 && adj && verb && shoes) {
-            myResult = `Yay!  ${noun1} will be wearing a ${noun2} top, ${adj} bottoms, 
+            myResult = `<h2>Yay!</h2>  ${noun1} will be wearing a ${noun2} top, ${adj} bottoms, 
             and carrying a ${verb} bag to the party. Oh! by the way, ${noun1} is going to 
             put on some ${shoes} shoes before leaving to the party. The party is going 
             to be fun!`;
