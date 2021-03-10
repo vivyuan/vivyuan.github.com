7e2c55440a39d62ae34640338ce0c4ffaa9e368f
vivyuan <moyuan@ucdavis.edu>
2021-03-10 11:44:46 -0800

    final2

--- des157/final/finalproject/script.js
+++ des157/final/finalproject/script.js
@@ -4,6 +4,7 @@
     const form1 = document.querySelector('#form1');
     const article1= document.querySelector('#article1');
     const playAgain = document.querySelector('#playAgain');
+    const resultSound = new Audio('media/resultmusic.mp3');
     let eachField= [];
 
     form1.addEventListener('submit', function(event){
@@ -17,6 +18,7 @@
 
         // Below are for the results
         if(noun1 && noun2 && adj && verb && shoes) {
+            resultSound.play();
             myResult = `<h2>Yay!</h2>  ${noun1} will be wearing a ${noun2} top, ${adj} bottoms, 
             and carrying a ${verb} purse to the party. Oh! by the way, ${noun1} is going to 
             put on some ${shoes} shoes before leaving to the party. The party is going 
@@ -40,6 +42,8 @@
     });
 
     playAgain.addEventListener('click', function(event){
+        resultSound.pause();
+        resultSound.currentTime = 0;
         event.preventDefault();
         document.getElementById("article1").style.visibility = "hidden";
         document.getElementById("before").style.visibility = "visible";
